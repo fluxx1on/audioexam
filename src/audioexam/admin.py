14a0ccd2f94cb46807349429c6956de3803b7e07
kiosura <samotokhin.art@yandex.ru>
2023-03-23 04:03:19 +0300

    initial

--- src/audioexam/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.shortcuts import render, HttpResponseRedirect
-from .models import *
-
-admin.site.register(Document)
-admin.site.register(Audio)
--- src/audioexam/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AudioexamConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'audioexam'
